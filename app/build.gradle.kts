d5cc94ef178dec954b94f0a4d128e39bb553e2a1
Wujumind <kennywujumind@gmail.com>
2026-05-11 19:33:14 -0400

    Reset versioning to v0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.pulsefit"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.4"
+        versionCode = 6
+        versionName = "0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
