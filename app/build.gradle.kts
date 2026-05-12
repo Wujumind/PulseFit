e15a0eaa11d716d846507f9fb3f9357e5ddea760
Wujumind <kennywujumind@gmail.com>
2026-05-11 20:31:06 -0400

    Replace default Android logo with PulseFit heart logo and update version to v0.95

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.pulsefit"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.6"
+        versionCode = 11
+        versionName = "0.95"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
